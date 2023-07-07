1eae2b0322bb630a3852ea7f5eab0e66654cefa4
Andres Moschini <andresmoschini@gmail.com>
2023-07-07 16:12:11 -0300

    test: add a test project

---
+++ DopplerBeplic.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace DopplerBeplic.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // Uncomment to test failing tests
+        // throw new NotImplementedException();
+    }
+}
---
+++ DopplerBeplic.Tests/Usings.cs
@@ -0,0 +1,4 @@
+global using Microsoft.AspNetCore.Mvc.Testing;
+global using System.Net;
+global using Xunit;
+global using Xunit.Abstractions;
