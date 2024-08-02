463c1b3ab2e85d3f48b3fc94f3fc7d2be99d4b18
mmosquera <mmosquera@makingsense.com>
2024-08-02 10:30:59 -0300

    feat: add responses

---
+++ doppler-beplic/Models/Responses/BeplicResponses/BeplicRoomResponse.cs
@@ -0,0 +1,10 @@
+namespace DopplerBeplic.Models.Responses.BeplicResponses
+{
+    public class BeplicRoomResponse
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Phone { get; set; }
+        public int Channel { get; set; }
+    }
+}
