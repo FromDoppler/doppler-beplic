c0096ddf521898ad3a2fb2b6d179554d219c7ae3
mmosquera <mmosquera@makingsense.com>
2024-08-02 12:59:24 -0300

    feat: add responses

---
+++ doppler-beplic/Models/Responses/TemplateResponse.cs
@@ -0,0 +1,16 @@
+namespace DopplerBeplic.Models.Responses
+{
+    public class TemplateResponse
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Language { get; set; }
+        public string? Category { get; set; }
+        public string? BodyText { get; set; }
+        public string? HeaderType { get; set; }
+        public string? HeaderText { get; set; }
+        public int HeaderAmount { get; set; }
+        public int BodyAmount { get; set; }
+        public string? FooterText { get; set; }
+    }
+}
