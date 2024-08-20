65a874035bdad3a4f7dd070a0425b4b103c9ae6e
mmosquera <mmosquera@makingsense.com>
2024-08-20 01:09:35 -0300

    chore: add missing properties on template response

--- doppler-beplic/Models/Responses/BeplicResponses/BeplicTemplateResponse.cs
+++ doppler-beplic/Models/Responses/BeplicResponses/BeplicTemplateResponse.cs
@@ -2,15 +2,33 @@ namespace DopplerBeplic.Models.Responses.BeplicResponses
 {
     public class BeplicTemplateResponse
     {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Language { get; set; }
-        public string? Category { get; set; }
-        public string? BodyText { get; set; }
-        public string? HeaderType { get; set; }
-        public string? HeaderText { get; set; }
-        public int? HeaderAmount { get; set; }
         public int? BodyAmount { get; set; }
+        public string? BodyText { get; set; }
+        public string? BotonPhone { get; set; }
+        public string? BotonUrl { get; set; }
+        public string? Category { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int? FieldId { get; set; }
         public string? FooterText { get; set; }
+        public int HasVar { get; set; }
+        public int? HeaderAmount { get; set; }
+        public string? HeaderText { get; set; }
+        public string? HeaderType { get; set; }
+        public int Id { get; set; }
+        public string? LabelPhone { get; set; }
+        public string? LabelUrl { get; set; }
+        public string? Language { get; set; }
+        public string? Name { get; set; }
+        public string? Parameter1Body { get; set; }
+        public string? Parameter2Body { get; set; }
+        public string? Parameter3Body { get; set; }
+        public string? ParameterHeader { get; set; }
+        public string? QuickReply1 { get; set; }
+        public string? QuickReply2 { get; set; }
+        public string? QuickReply3 { get; set; }
+        public bool StateDelete { get; set; }
+        public string? Status { get; set; }
+        public string? UuidTemplate { get; set; }
+
     }
 }
